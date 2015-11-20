7b9558ed772343ead96250b98c10ec47cba3c82c
John Skufca <john.skufca@imulus.com>
2015-11-20 07:57:15 -0700

    More Updates to Homepage and Sub-Sites

---
+++ src/USDA-ARS.Umbraco.Extensions/Helpers/LinqExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDA_ARS.Umbraco.Extensions.Helpers
+{
+    public static class LinqExtensions
+    {
+        public static List<IEnumerable<T>> Split<T>(this List<T> list, int parts)
+        {
+            int i = 0;
+            var splits = from item in list
+                         group item by i++ % parts into part
+                         select part.AsEnumerable();
+            return splits.ToList();
+        }
+    }
+}
--- src/USDA-ARS.Umbraco.Extensions/Helpers/Nodes.cs
+++ src/USDA-ARS.Umbraco.Extensions/Helpers/Nodes.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Umbraco.Core.Models;
 using Umbraco.Web;
+using USDA_ARS.Umbraco.Extensions.Models;
+using USDA_ARS.Umbraco.Extensions.Helpers;
 
 namespace USDA_ARS.Umbraco.Extensions.Helpers
 {
@@ -28,12 +30,92 @@ namespace USDA_ARS.Umbraco.Extensions.Helpers
             return mainNav.Children;
         }
 
+        public static IEnumerable<IPublishedContent> MainOfficeList()
+        {
+            IPublishedContent mainList = SiteSettings().Children.FirstOrDefault(n => n.IsDocumentType("MainOfficeList"));
+
+            return mainList.Children;
+        }
+
         public static IEnumerable<IPublishedContent> RegionSiteList()
         {
             IPublishedContent ArsLocations = UmbHelper.TypedContentAtRoot().FirstOrDefault(n => n.IsDocumentType("ARSLocations"));
 
             return ArsLocations.Children;
         }
+
+        public static IEnumerable<IPublishedContent> RegionCityList(IPublishedContent region)
+        {
+            return region.Children;
+        }
+
+        public static void CityResearchUnitList(ref List<ResearchUnitDisplay> researchUnits, IPublishedContent city, int level)
+        {
+            IEnumerable<IPublishedContent> researchUnitList = city.Children;
+
+            foreach (IPublishedContent unit in researchUnitList)
+            {
+                ResearchUnitDisplay researchUnitDisplay = new ResearchUnitDisplay();
+
+                researchUnitDisplay.ResearchUnit = unit;
+                researchUnitDisplay.Level = level;
+
+                researchUnits.Add(researchUnitDisplay);
+
+                CityResearchUnitList(ref researchUnits, unit, level + 1);
+            }
+        }
+
+        public static List<List<ResearchUnitDisplay>> ResearchUnitsSplit(List<ResearchUnitDisplay> researchUnits)
+        {
+            List<List<ResearchUnitDisplay>> unitsSplits = new List<List<ResearchUnitDisplay>>();
+            List<ResearchUnitDisplay> unitsOneList = new List<ResearchUnitDisplay>();
+            List<ResearchUnitDisplay> unitsTwoList = new List<ResearchUnitDisplay>();
+
+            int i = 0;
+            int mid = researchUnits.Count / 2;
+
+            foreach(ResearchUnitDisplay unit in researchUnits)
+            {
+                if (i < mid)
+                {
+                    unitsOneList.Add(unit);
+                }
+                else
+                {
+                    unitsTwoList.Add(unit);
+                }
+
+                i++;
+            }
+
+            unitsSplits.Add(unitsOneList);
+            unitsSplits.Add(unitsTwoList);
+
+            return unitsSplits;
+        }
+
+
+        public static List<string> StateListFromRegion(IPublishedContent region)
+        {
+            List<string> stateList = new List<string>();
+
+            IEnumerable<IPublishedContent> cityList = region.Children;
+
+            foreach (IPublishedContent city in cityList)
+            {
+                stateList.Add(city.GetPropertyValue<string>("state"));
+            }
+
+            if (stateList != null && stateList.Count > 0)
+            {
+                stateList = stateList.Distinct().ToList();
+            }
+
+            return stateList;
+        }
+
+
 
     }
 }
