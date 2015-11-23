ab9e27f172fc9d32e569cc1e24392a27a3c1a158
phanikgithub <phanik.chavali@gmail.com>
2015-11-23 12:12:44 -0500

    added column numbers to avoid confusion

--- src/USDA-ARS.Core/SQLDB/usp_InsertNewMessagesToNews.sql
+++ src/USDA-ARS.Core/SQLDB/usp_InsertNewMessagesToNews.sql
@@ -18,42 +18,70 @@ declare @maxMessageId INT;
 set  @maxMessageId=(	select	max(MessageID) as MessageID	from sitepublisherii.dbo.NewsTemp
 	where 	ToField = 'ARS News subscriber <spNewsFeeder@ars.usda.gov>')
 CREATE TABLE #TempTable(
+--1
  DateField datetime,
+ --2
  FromField nvarchar(max),
+ --3
  ToField nvarchar(max),
+ --4
  ReplytoField nvarchar(max),
+ --5
  MessageNumberField nvarchar(max),
+ --6
  SubjectField nvarchar(max),
+ --7
  BodyField nvarchar(max),
+ --8
  HeaderField nvarchar(max),
+ --9
  originsite_type nvarchar(max),
+ --10
  originsite_id nvarchar(max),
+ --11
  published nvarchar(max),
+ --12
  MessageID nvarchar(max),
+ --13
  ISFileName nvarchar(max),
  )
 	insert into #TempTable
 		select  
+		--1
 		CreatStamp_,
+		--2
 		HdrFrom_,
+		--3
 		'ARS News subscriber <spNewsFeeder@ars.usda.gov>',
+		--4
 		HdrFrom_,
+		--5
 		'1',
+		--6
 		HdrSubject_,
+		--7
 		Body_,
+		--8
 		HdrAll_,
+		--9
 		'place',
+		--10
 		'01040000',
-		'01040000',	
+			
+		--11
 		'p',
+		--12
 		MessageID_,
+		--13
 		convert(varchar(10), CreatStamp_, 120)+'.htm'	
 		from Lyris.dbo.messages_
 	
 	where		MessageID_ > @maxMessageId	
+	and HdrTo_  = 'ARS News subscriber <spNewsFeeder@ars.usda.gov>'
 
 	select * from #TempTable
-
+	--truncate table #temptable
+	--end
 	INSERT INTO sitepublisherii.dbo.NewsTemp 
 					(
 						DateField, 
@@ -85,5 +113,6 @@ CREATE TABLE #TempTable(
 						ISFileName		
 						from #TempTable						-- chg-01
 
-	truncate table #temptable
+	--truncate table #temptable
 END
+--exec [dbo].[usp_GetLatestNewsFromLyrisMessages]
