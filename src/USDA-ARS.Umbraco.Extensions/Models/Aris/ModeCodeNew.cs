7d49143460bb278b5e7f246ce052f51db318aab5
phanikgithub <phanik.chavali@gmail.com>
2015-12-16 13:04:03 -0500

    commiting umbraco files

---
+++ src/USDA-ARS.Umbraco.Extensions/Models/Aris/ModeCodeNew.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence;
+
+namespace USDA_ARS.Umbraco.Extensions.Models.Aris
+{
+    [TableName("NewModecodes")]
+    public class ModeCodeNew
+    {
+        [Column("OldModecode")]
+        public string ModecodeOld { get; set; }
+
+        [Column("NewModecode")]
+        public string ModecodeNew { get; set; }
+    }
+}
