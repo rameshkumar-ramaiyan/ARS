91dffee1fe451ef8223c050d5ffec0f675a04982
John Skufca <john.skufca@imulus.com>
2016-03-03 09:13:29 -0700

    Initial People Insert Method

---
+++ src/USDA-ARS.LocationsWebApp/DL/AddPersonSite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace USDA_ARS.LocationsWebApp.DL
+{
+    public class AddPersonSite
+    {
+
+
+
+    }
+}
