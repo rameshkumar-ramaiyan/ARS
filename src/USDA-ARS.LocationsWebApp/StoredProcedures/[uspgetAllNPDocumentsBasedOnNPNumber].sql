aa0be5b928524a3780b7d6f3d47c0e29e7dd4822
phanikgithub <phanik.chavali@gmail.com>
2016-04-18 07:33:38 -0400

    added np docs sps

--- src/USDA-ARS.LocationsWebApp/StoredProcedures/[uspgetAllNPDocumentsBasedOnNPNumber].sql
+++ src/USDA-ARS.LocationsWebApp/StoredProcedures/[uspgetAllNPDocumentsBasedOnNPNumber].sql
@@ -1,45 +1,88 @@
 USE [aris_public_webNew]
 GO
-
-/****** Object:  StoredProcedure [dbo].[uspgetAllNPDocumentsBasedOnNPNumber]    Script Date: 4/11/2016 7:13:24 AM ******/
+/****** Object:  StoredProcedure [dbo].[uspgetAllNPDocumentsBasedOnNPNumber]    Script Date: 4/17/2016 7:05:33 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE PROCEDURE [dbo].[uspgetAllNPDocumentsBasedOnNPNumber]
+ALTER PROCEDURE [dbo].[uspgetAllNPDocumentsBasedOnNPNumber]
 
 @NPNumber nvarchar(max) 
 AS
  
 BEGIN
 	
-	DECLARE  
-			@DocPage		nvarchar(max)
+	
+	
+--	DECLARE  
+--			@DocPage		nvarchar(max)
 			
+--select  @DocPage= cast( DocPage as nvarchar(max)) from  sitepublisherii.dbo.DocPages where DocVer_ID =(select currentversion_id from sitepublisherii.dbo.Documents
+-- where 
+-- --Title not like '%Strategic Vision%' and 
+-- rtrim(ltrim(OriginSite_ID))=rtrim(ltrim(@NPNumber) )and 
+-- published =  'p'and SPSysEndTime is  null
+-- )
+----179
+--and CurrentVersion = 1
 
-select  @DocPage= cast( DocPage as nvarchar(max)) from  sitepublisherii.dbo.DocPages where DocVer_ID =(select currentversion_id from sitepublisherii.dbo.Documents
- where Title not like '%Strategic Vision%' and rtrim(ltrim(OriginSite_ID))=rtrim(ltrim(@NPNumber) )and published =  'p'and SPSysEndTime is  null)
---179
-and CurrentVersion = 1
+-- select CAST(CAST(N'' AS XML).value('(sql:variable("@DocPage"))',
+--           'VARBINARY(MAX)') AS VARCHAR(MAX)) 
+--		   AS [DocPageContent]
+
+
+select DocId,Title,CurrentVersion_ID,DocType,OriginSite_ID,oldURL,
+sitepublisherii.dbo.DocPages.DocPage
+--CAST(CAST(N'' AS XML).value('(sql:variable("@DocPage"))',
+--           'VARBINARY(MAX)') AS VARCHAR(MAX)) 
+--		   AS [DocPageContent]
+from sitepublisherii.dbo.Documents 
+join sitepublisherii.dbo.DocPages
+on sitepublisherii.dbo.Documents.CurrentVersion_ID=sitepublisherii.dbo.DocPages.DocVer_Id
+ where 
+ OriginSite_ID =  '107'
+ and published =  'p'and sitepublisherii.dbo.Documents.SPSysEndTime is  null
+ and sitepublisherii.dbo.DocPages.CurrentVersion=1
+-- and DocType='Program Planning'                                                                                    
+--and Title   like '%Action Plans%' 
+order by DocType
  
- --select CAST(CAST(N'' AS XML).value('(sql:variable("@DocPage"))',
- --          'VARBINARY(MAX)') AS VARCHAR(MAX)) 
-	--	   AS [DocPageContent]
+
 END
 
  -- all pages for NP 107
 	--0.select * from sitepublisherii.dbo.NPGroups order by NPGroupID
 	--1.select * from sitepublisherii.dbo.NPrograms  where npgroup_id=1 order by npgroup_id,NPNumber	
 	--2.select * from sitepublisherii.dbo.NPrograms order by npgroup_id,NPNumber	
---3.select * from  sitepublisherii.dbo.Documents where OriginSite_ID =  '107'and published =  'p'and SPSysEndTime is  nul
---4.select * from  sitepublisherii.dbo.DocPages where DocVer_ID !=(179)and CurrentVersion = 1
+--3.select * from  sitepublisherii.dbo.Documents where OriginSite_ID =  '107'and published =  'p'and SPSysEndTime is  null
+--4.select * from  sitepublisherii.dbo.DocPages where DocVer_ID =(22894)and CurrentVersion = 1
 
 --select * from sitepublisherii.dbo.Documents
--- where OriginSite_ID =  '107'and published =  'p'and SPSysEndTime is  null
+-- where OriginSite_ID =  '107'and published =  'p'and SPSysEndTime is  null order by CurrentVersion
 --and Title  not like '%Strategic Vision%' 
 --order by DocType
-GO
+
+
+--BEGIN
+	
+--	DECLARE  
+--			@DocPage		nvarchar(max)
+			
+
+--select  @DocPage= cast( DocPage as nvarchar(max)) from  sitepublisherii.dbo.DocPages where DocVer_ID =(select currentversion_id from sitepublisherii.dbo.Documents
+-- where 
+-- Title not like '%Strategic Vision%' and 
+-- rtrim(ltrim(OriginSite_ID))=rtrim(ltrim(@NPNumber) )and 
+-- published =  'p'and SPSysEndTime is  null
+-- )
+----179
+--and CurrentVersion = 1
+ 
+-- select CAST(CAST(N'' AS XML).value('(sql:variable("@DocPage"))',
+--           'VARBINARY(MAX)') AS VARCHAR(MAX)) 
+--		   AS [DocPageContent]
+--END
+
+
 
 
