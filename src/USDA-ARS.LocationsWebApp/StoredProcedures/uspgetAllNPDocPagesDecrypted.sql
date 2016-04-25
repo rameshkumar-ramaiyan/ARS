ea7a49dbb3836b3e669039055ed3583b67afa933
phanikgithub <phanik.chavali@gmail.com>
2016-04-25 08:07:22 -0400

    added nppagesdecryption sp

---
+++ src/USDA-ARS.LocationsWebApp/StoredProcedures/uspgetAllNPDocPagesDecrypted.sql
@@ -0,0 +1,39 @@
+USE [aris_public_webNew]
+GO
+
+/****** Object:  StoredProcedure [dbo].[uspgetAllNPDocPagesDecrypted]    Script Date: 4/25/2016 8:04:08 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[uspgetAllNPDocPagesDecrypted]
+
+@DocPageEncrypted nvarchar(max) 
+AS
+ 
+BEGIN
+	
+	
+	
+
+
+ select CAST(CAST(N'' AS XML).value('(sql:variable("@DocPageEncrypted"))',
+           'VARBINARY(MAX)') AS VARCHAR(MAX)) 
+		   AS [DocPageContent]
+
+
+
+END
+
+ 
+
+
+
+
+
+GO
+
+
