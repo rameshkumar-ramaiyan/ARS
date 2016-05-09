93dbd262d6b0aec28ff172d05eb54ba6802090fc
phanikgithub <phanik.chavali@gmail.com>
2016-05-09 19:33:11 -0400

    SP2UserFiles/Place/  renamed to  ARSUserFiles/

--- src/USDA-ARS.LocationsWebApp/AddUpdateLocations.aspx.cs
+++ src/USDA-ARS.LocationsWebApp/AddUpdateLocations.aspx.cs
@@ -364,7 +364,7 @@ namespace USDA_ARS.LocationsWebApp
                     fieldsetFiles.Disabled = false;
                     fieldsetFiles.Id = Guid.NewGuid();
                     fieldsetFiles.Properties = new List<Property>();
-                    fieldsetFiles.Properties.Add(new Property("file", "/SP2UserFiles/Place/20000000/software/Brio-Insight_en.zip")); // set the file path
+                    fieldsetFiles.Properties.Add(new Property("file", "/ARSUserFiles/20000000/software/Brio-Insight_en.zip")); // set the file path
 
                     softwareFilesList.Fieldsets.Add(fieldsetFiles);
 
@@ -403,7 +403,7 @@ namespace USDA_ARS.LocationsWebApp
             fieldsetCar.Id = Guid.NewGuid();
             fieldsetCar.Properties = new List<Property>();
             fieldsetCar.Properties.Add(new Property("slideName", "ARS Commitment")); // set the slide name
-            fieldsetCar.Properties.Add(new Property("slideImage", "/SP2UserFiles/Place/00000000/images/PhotoCarousel/ARS1890-B.png")); // set the slide image path
+            fieldsetCar.Properties.Add(new Property("slideImage", "/ARSUserFiles/00000000/images/PhotoCarousel/ARS1890-B.png")); // set the slide image path
             fieldsetCar.Properties.Add(new Property("slideText", "<p>Slide Text Here</p>")); // set the slide html text
             fieldsetCar.Properties.Add(new Property("slideAltText", "Slide Alt Text Here")); // set the slide alt text
 
@@ -795,9 +795,9 @@ namespace USDA_ARS.LocationsWebApp
                     string slideAltText = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(3);
                     string slideURL = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(2);
                     string slideImage = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(1);
-                    string slideImageSP2 = "/SP2UserFiles/Place/" + "" + completeModeCode.Replace("-", "") + "/images/PhotoCarousel/" + slideImage;
+                    string slideImageSP2 = "/ARSUserFiles/" + "" + completeModeCode.Replace("-", "") + "/images/PhotoCarousel/" + slideImage;
                     string slideFilePath = null;
-                    if (slideURL.Contains("/SP2UserFiles/Place/"))
+                    if (slideURL.Contains("/ARSUserFiles/"))
                     {
                         slideFilePath = slideImageSP2; // If a slide links to a file instead of a page, set it here.
                     }
@@ -904,7 +904,7 @@ namespace USDA_ARS.LocationsWebApp
                                 fieldsetFiles.Disabled = false;
                                 fieldsetFiles.Id = Guid.NewGuid();
                                 fieldsetFiles.Properties = new List<Property>();
-                                string filePathSP2 = "/SP2UserFiles/Place/" + "" + completeModeCode.Replace("-", "") + "/software/" + filePathSP2List[filePathSP2ListRowId];
+                                string filePathSP2 = "/ARSUserFiles/" + "" + completeModeCode.Replace("-", "") + "/software/" + filePathSP2List[filePathSP2ListRowId];
                                 fieldsetFiles.Properties.Add(new Property("file", filePathSP2)); // set the file path
                                 softwareFilesList.Fieldsets.Add(fieldsetFiles);
                             }
@@ -1161,9 +1161,9 @@ namespace USDA_ARS.LocationsWebApp
                         string slideAltText = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(3);
                         string slideURL = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(2);
                         string slideImage = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(1);
-                        string slideImageSP2 = "/SP2UserFiles/Place/" + "" + newModeCodeProperty.Replace("-", "") + "/images/PhotoCarousel/" + slideImage;
+                        string slideImageSP2 = "/ARSUserFiles/" + "" + newModeCodeProperty.Replace("-", "") + "/images/PhotoCarousel/" + slideImage;
                         string slideFilePath = null;
-                        if (slideURL.Contains("/SP2UserFiles/Place/"))
+                        if (slideURL.Contains("/ARSUserFiles/"))
                         {
                             slideFilePath = slideImageSP2; // If a slide links to a file instead of a page, set it here.
                         }
@@ -1247,7 +1247,7 @@ namespace USDA_ARS.LocationsWebApp
                         fieldsetSoftware.Properties.Add(new Property("shortBlurb", shortBlurb)); // set the short blurb
                         fieldsetSoftware.Properties.Add(new Property("info", info)); // set the large text information
                                                                                      // Files
-                        string filePathSP2 = "/SP2UserFiles/Place/" + "" + newModeCodeProperty.Replace("-", "") + "/software/Brio-Insight_en.zip";
+                        string filePathSP2 = "/ARSUserFiles/" + "" + newModeCodeProperty.Replace("-", "") + "/software/Brio-Insight_en.zip";
                         {
                             ApiArchetype softwareFilesList = new ApiArchetype();
 
@@ -1413,9 +1413,9 @@ namespace USDA_ARS.LocationsWebApp
                         string slideAltText = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(3);
                         string slideURL = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(2);
                         string slideImage = legacyCarouselSlidesBeforeInsertion.Rows[legacyCarouselSlidesRowId].Field<string>(1);
-                        string slideImageSP2 = "/SP2UserFiles/Place/" + "" + newModeCodeProperty.Replace("-", "") + "/images/PhotoCarousel/" + slideImage;
+                        string slideImageSP2 = "/ARSUserFiles/" + "" + newModeCodeProperty.Replace("-", "") + "/images/PhotoCarousel/" + slideImage;
                         string slideFilePath = null;
-                        if (slideURL.Contains("/SP2UserFiles/Place/"))
+                        if (slideURL.Contains("/ARSUserFiles/"))
                         {
                             slideFilePath = slideImageSP2; // If a slide links to a file instead of a page, set it here.
                         }
@@ -1502,7 +1502,7 @@ namespace USDA_ARS.LocationsWebApp
                         fieldsetSoftware.Properties.Add(new Property("info", info)); // set the large text information
                         List<string> filePathSP2List = new List<string>();
                         // Files
-                        string filePathSP2 = "/SP2UserFiles/Place/" + "" + newModeCodeProperty.Replace("-", "") + "/software/Brio-Insight_en.zip";
+                        string filePathSP2 = "/ARSUserFiles/" + "" + newModeCodeProperty.Replace("-", "") + "/software/Brio-Insight_en.zip";
                         {
                             ApiArchetype softwareFilesList = new ApiArchetype();
 
