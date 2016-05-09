66b5bdd5b77ce29716f219e73a472df82fbdb460
John Skufca <john.skufca@imulus.com>
2016-05-09 04:57:22 -0600

    Forgot to commit my bug fixes

--- src/USDA-ARS.Umbraco.Extensions/Helpers/Nodes.cs
+++ src/USDA-ARS.Umbraco.Extensions/Helpers/Nodes.cs
@@ -65,11 +65,11 @@ namespace USDA_ARS.Umbraco.Extensions.Helpers
 
         public static IEnumerable<IPublishedContent> NationalProgramsList()
         {
-            IPublishedContent NationalProgramsRoot = UmbHelper.TypedContentAtRoot().FirstOrDefault(n => n.IsDocumentType("NationalProgramsRoot"));
+            IPublishedContent NationalProgramsRoot = UmbHelper.TypedContentAtRoot().FirstOrDefault(n => n.IsDocumentType("NationalProgramMain"));
 
             if (NationalProgramsRoot != null)
             {
-                return NationalProgramsRoot.Children.FirstOrDefault().Children;
+                return NationalProgramsRoot.Children;
             }
 
             return null;
