3c1ac0d35170eb16cb35db4331a26ef4feefac3b
John Skufca <john.skufca@imulus.com>
2016-05-11 22:35:52 -0600

    Committing Info Staff Import

---
+++ src/USDA-ARS.ImportInfoStaff/Models/InfoStaffPathLookup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDA_ARS.ImportInfoStaff.Models
+{
+    public class InfoStaffPathLookup
+    {
+        public string Path { get; set; }
+        public int UmbracoId { get; set; }
+    }
+}
---
+++ src/USDA-ARS.ImportInfoStaff/Models/PageImport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDA_ARS.ImportInfoStaff.Models
+{
+    public class PageImport
+    {
+        public string Title { get; set; }
+        public string BodyText { get; set; }
+        public string OldPath { get; set; }
+    }
+}
