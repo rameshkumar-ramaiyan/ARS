506aa7f6744dc11aa4cd4b046e5def62d67a8cbc
John Skufca <john.skufca@imulus.com>
2016-06-20 12:03:44 -0600

    Updated Active Directory Code

---
+++ src/ARS.AuthenticationProvider/BackofficeMembershipProviderCustomOwinStartup.cs
@@ -0,0 +1,53 @@
+﻿using ARS.AuthenticationProvider;
+using Microsoft.Owin;
+using Owin;
+using Umbraco.Core;
+using Umbraco.Core.Models.Identity;
+using Umbraco.Core.Security;
+using Umbraco.Web.Security.Identity;
+
+//To use this startup class, change the appSetting value in the web.config called 
+// "owin:appStartup" to be "BackofficeMembershipProviderCustomOwinStartup"
+
+[assembly: OwinStartup("BackofficeMembershipProviderCustomOwinStartup", typeof(BackofficeMembershipProviderCustomOwinStartup))]
+
+namespace ARS.AuthenticationProvider
+{
+    /// <summary>
+    /// A custom way to configure OWIN for Umbraco
+    /// </summary>
+    /// <remarks>
+    /// The startup type is specified in appSettings under owin:appStartup - change it to "BackofficeMembershipProviderCustomOwinStartup" to use this class
+    /// 
+    /// This startup class would allow you to customize the Identity IUserStore and/or IUserManager for the Umbraco Backoffice
+    /// </remarks>
+    public class BackofficeMembershipProviderCustomOwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Configure the Identity user manager for use with Umbraco Back office
+
+            var applicationContext = ApplicationContext.Current;
+            app.ConfigureUserManagerForUmbracoBackOffice<BackOfficeUserManager, BackOfficeIdentityUser>(
+                applicationContext,
+                (options, context) =>
+                {
+                    var membershipProvider = MembershipProviderExtensions.GetUsersMembershipProvider().AsUmbracoMembershipProvider();
+                    var userManager = BackOfficeUserManager.Create(options,
+                        applicationContext.Services.UserService,
+                        applicationContext.Services.ExternalLoginService,
+                        membershipProvider);
+
+                    // Call custom passowrd checker.
+                    userManager.BackOfficeUserPasswordChecker = new BackofficeMembershipProviderPasswordChecker();
+
+                    return userManager;
+                });
+
+            //Ensure owin is configured for Umbraco back office authentication
+            app
+                .UseUmbracoBackOfficeCookieAuthentication(ApplicationContext.Current)
+                .UseUmbracoBackOfficeExternalCookieAuthentication(ApplicationContext.Current);
+        }
+    }
+}
