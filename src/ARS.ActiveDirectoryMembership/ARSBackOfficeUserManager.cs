61e025d20b4484df5d10e15e52a8faacf4186d32
John Skufca <john.skufca@imulus.com>
2016-07-03 12:17:24 -0600

    Implement Updated Active Directory Code to /develop and /staging branches

---
+++ src/ARS.ActiveDirectoryMembership/ARSBackOfficeUserManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.DirectoryServices;
+using System.Threading.Tasks;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models.Identity;
+using Umbraco.Core.Security;
+
+namespace ARS.ActiveDirectoryMembership
+{
+    public class ARSBackOfficeUserManager : IBackOfficeUserPasswordChecker
+    {
+        public Task<BackOfficeUserPasswordCheckerResult> CheckPasswordAsync(BackOfficeIdentityUser user, string password)
+        {
+            var result = LdapAuth(user.UserName, password)
+                ? Task.FromResult(BackOfficeUserPasswordCheckerResult.ValidCredentials)
+                : Task.FromResult(BackOfficeUserPasswordCheckerResult.FallbackToDefaultChecker);
+
+            return result;
+        }
+
+        private bool LdapAuth(string username, string password)
+        {
+            bool resp = false;
+            try
+            {
+                string ldapRoot = ConfigurationManager.ConnectionStrings["ADConnectionString"].ConnectionString;
+                var entry = new DirectoryEntry(ldapRoot, username, password);
+                try
+                {
+                    var search = new DirectorySearcher(entry) { Filter = "(SAMAccountName=" + username + ")" };
+                    search.PropertiesToLoad.Add("cn");
+                    SearchResult result = search.FindOne();
+                    if (result != null)
+                    {
+                        // Login was successful
+                        resp = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Warn(typeof(ARSUserManager), "Problem with login: " + username + " | " + ex.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Warn(typeof(ARSUserManager), "Problem with login: " + username + " | " + ex.ToString());
+            }
+
+            return resp;
+        }
+    }
+}
---
+++ src/USDA-ARS.Umbraco/App_Start/ARSUmbracoBackofficeADOwinStartup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Umbraco.Web.Security.Identity;
+using Umbraco.IdentityExtensions;
+using USDA_ARS.Umbraco;
+using Umbraco.Core.Models.Identity;
+using ARS.ActiveDirectoryMembership;
+using Umbraco.Core;
+using Umbraco.Core.Security;
+
+[assembly: OwinStartup("ARSUmbracoBackofficeADOwinStartup", typeof(ARSUmbracoBackofficeADOwinStartup))]
+
+namespace USDA_ARS.Umbraco
+{
+    public class ARSUmbracoBackofficeADOwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var applicationContext = ApplicationContext.Current;
+
+            app.ConfigureUserManagerForUmbracoBackOffice<BackOfficeUserManager, BackOfficeIdentityUser>(
+                applicationContext,
+                (options, context) =>
+                {
+                    var membershipProvider = MembershipProviderExtensions.GetUsersMembershipProvider().AsUmbracoMembershipProvider();
+                    var userManager = BackOfficeUserManager.Create(options,
+                        applicationContext.Services.UserService,
+                        applicationContext.Services.ExternalLoginService,
+                        membershipProvider);
+
+                    //Set your own custom IBackOfficeUserPasswordChecker   
+                    userManager.BackOfficeUserPasswordChecker = new ARSBackOfficeUserManager();
+
+                    return userManager;
+                });
+
+            app.UseUmbracoBackOfficeCookieAuthentication(ApplicationContext.Current)
+                .UseUmbracoBackOfficeExternalCookieAuthentication(ApplicationContext.Current);
+        }
+    }
+}
