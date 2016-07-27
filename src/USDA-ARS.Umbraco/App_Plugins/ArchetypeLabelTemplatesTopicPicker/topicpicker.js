57522bc7057c58dd2aa76d6859e4c8bfedfdbc9e
Xorcery <github@xorcore.com>
2016-07-27 07:34:56 -0600

    Temp Fix for Nav Picker

--- src/USDA-ARS.Umbraco/App_Plugins/ArchetypeLabelTemplatesTopicPicker/topicpicker.js
+++ src/USDA-ARS.Umbraco/App_Plugins/ArchetypeLabelTemplatesTopicPicker/topicpicker.js
@@ -1,32 +1,12 @@
 ﻿var TopicPickerTemplate = {};
 
 TopicPickerTemplate.getTitle = function (value, scope) {
-   if (id != null && id != '') {
+    //this is the property model
+    var output = value;
 
-      var cachedValue = _.find(TopicPickerTemplate.MyPrevalues, function (prevalue) {
-         return prevalue.id == id;
-      });
-
-      if (cachedValue) {
-         return cachedValue.value;
-      }
-
-      $.ajax({
-         url: '/umbraco/usda/TopicPicker/GetTitle/' + id,
-         type: 'GET',
-         dataType: 'json',
-         async: true
-      }).success(function (data) {
-         NewsTopicPickerTemplate.MyPrevalues.push({ id: id, value: data });
-         return data;
-      });
-
-      return "Loading...";
-
-   } else {
-      return " - ";
-   }
-
+    if (value) {
+        return value.Text;
+	}
 
     //if you wanted to get the name of the content instead, you'd have to get it from the server here since it's not in the model
     return "";
