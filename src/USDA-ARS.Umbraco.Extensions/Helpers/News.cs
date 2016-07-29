77be3157eeea402d23c40896e2d229bdd477c093
Xorcery <github@xorcore.com>
2016-07-29 08:53:14 -0600

    Fix News Topics for RSS

--- src/USDA-ARS.Umbraco.Extensions/Helpers/News.cs
+++ src/USDA-ARS.Umbraco.Extensions/Helpers/News.cs
@@ -14,162 +14,167 @@ using USDA_ARS.Umbraco.Extensions.Models.Aris;
 
 namespace USDA_ARS.Umbraco.Extensions.Helpers
 {
-    public class News
-    {
-        public static List<IPublishedContent> GetNews(int newsCount, string modeCode = null, DateTime? dateStart = null, DateTime? dateEnd = null)
-        {
-            List<IPublishedContent> newsItems = null;
+   public class News
+   {
+      public static List<IPublishedContent> GetNews(int newsCount, string modeCode = null, DateTime? dateStart = null, DateTime? dateEnd = null)
+      {
+         List<IPublishedContent> newsItems = null;
 
-            newsItems = Helpers.Nodes.NewsList();
+         newsItems = Helpers.Nodes.NewsList();
 
-            if (dateStart != null && dateEnd != null)
+         if (dateStart != null && dateEnd != null)
+         {
+            newsItems = newsItems.Where(p => p.GetPropertyValue<DateTime>("articleDate") >= dateStart && p.GetPropertyValue<DateTime>("articleDate") <= dateEnd).ToList();
+         }
+
+         newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
+
+         newsItems = newsItems.Take(newsCount).ToList();
+
+         return newsItems;
+      }
+
+
+      public static List<IPublishedContent> GetNewsByProduct(string productName, int newsCount, string modeCode = null, DateTime? dateStart = null, DateTime? dateEnd = null)
+      {
+         List<IPublishedContent> newsItems = null;
+
+         List<NewsProductItem> newsProductList = GetNewsProductList();
+
+         if (newsProductList != null && newsProductList.Count > 0)
+         {
+            NewsProductItem newsProductFound = newsProductList.Where(p => p.Text == productName).FirstOrDefault();
+
+            if (newsProductFound != null)
             {
-                newsItems = newsItems.Where(p => p.GetPropertyValue<DateTime>("articleDate") >= dateStart && p.GetPropertyValue<DateTime>("articleDate") <= dateEnd).ToList();
+               newsItems = Helpers.Nodes.NewsList();
+
+               newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsProductsList") != null &&
+                           p.GetPropertyValue<string>("newsProductsList").ToLower() == newsProductFound.Value.ToLower()).ToList();
+
+               if (dateStart != null && dateEnd != null)
+               {
+                  newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsProductsList") != null &&
+                          p.GetPropertyValue<string>("newsProductsList").ToLower() == newsProductFound.Value.ToLower() &&
+                          p.GetPropertyValue<DateTime>("articleDate") >= dateStart && p.GetPropertyValue<DateTime>("articleDate") <= dateEnd).ToList();
+               }
+
+               newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
+
+               newsItems = newsItems.Take(newsCount).ToList();
             }
+         }
 
-            newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
-
-            newsItems = newsItems.Take(newsCount).ToList();
-
-            return newsItems;
-        }
+         return newsItems;
+      }
 
 
-        public static List<IPublishedContent> GetNewsByProduct(string productName, int newsCount, string modeCode = null, DateTime? dateStart = null, DateTime? dateEnd = null)
-        {
-            List<IPublishedContent> newsItems = null;
+      public static List<IPublishedContent> GetNewsByTopic(string topicName, int newsCount, string modeCode = null, DateTime? dateStart = null, DateTime? dateEnd = null)
+      {
+         List<IPublishedContent> newsItems = null;
 
-            List<NewsProductItem> newsProductList = GetNewsProductList();
+         List<NewsTopicItem> newsTopicList = GetNewsTopicList();
 
-            if (newsProductList != null && newsProductList.Count > 0)
+         if (newsTopicList != null && newsTopicList.Count > 0)
+         {
+            NewsTopicItem newsTopicFound = newsTopicList.Where(p => p.Text == topicName).FirstOrDefault();
+
+            if (newsTopicFound != null)
             {
-                NewsProductItem newsProductFound = newsProductList.Where(p => p.Text == productName).FirstOrDefault();
+               newsItems = Helpers.Nodes.NewsList();
 
-                if (newsProductFound != null)
-                {
-                    newsItems = Helpers.Nodes.NewsList();
+               newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsTopicsList") != null).ToList();
 
-                    newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsProductsList") != null &&
-                                p.GetPropertyValue<string>("newsProductsList").ToLower() == newsProductFound.Value.ToLower()).ToList();
+               string newsTopicGuid = newsTopicFound.Value.ToLower();
 
-                    if (dateStart != null && dateEnd != null)
-                    {
-                        newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsProductsList") != null &&
-                                p.GetPropertyValue<string>("newsProductsList").ToLower() == newsProductFound.Value.ToLower() && 
-                                p.GetPropertyValue<DateTime>("articleDate") >= dateStart && p.GetPropertyValue<DateTime>("articleDate") <= dateEnd).ToList();
-                    }
+               //newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsTopicsList").ToLower().Contains(newsTopicGuid)).ToList();
 
-                    newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
+               if (dateStart != null && dateEnd != null)
+               {
+                  newsItems = newsItems.Where(p => p.GetProperty("newsTopicsList").DataValue != null &&
+                      p.GetProperty("newsTopicsList").DataValue.ToString().ToLower().Contains(newsTopicFound.Value.ToLower())).ToList();
 
-                    newsItems = newsItems.Take(newsCount).ToList();
-                }
+                  if (newsItems != null && newsItems.Any())
+                  {
+                     newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
+                     newsItems = newsItems.Take(20).ToList();
+                  }
+               }
+
+               newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
+
+               newsItems = newsItems.Take(newsCount).ToList();
             }
+         }
 
-            return newsItems;
-        }
+         return newsItems;
+      }
 
 
-        public static List<IPublishedContent> GetNewsByTopic(string topicName, int newsCount, string modeCode = null, DateTime? dateStart = null, DateTime? dateEnd = null)
-        {
-            List<IPublishedContent> newsItems = null;
+      public static List<NewsProductItem> GetNewsProductList()
+      {
+         List<NewsProductItem> productList = new List<NewsProductItem>();
+         IPublishedContent node = Helpers.Nodes.SiteSettings();
 
-            List<NewsTopicItem> newsTopicList = GetNewsTopicList();
-
-            if (newsTopicList != null && newsTopicList.Count > 0)
+         if (node != null)
+         {
+            if (node.HasValue("newsProducts"))
             {
-                NewsTopicItem newsTopicFound = newsTopicList.Where(p => p.Text == topicName).FirstOrDefault();
+               ArchetypeModel newsProducts = node.GetPropertyValue<ArchetypeModel>("newsProducts");
 
-                if (newsTopicFound != null)
-                {
-                    newsItems = Helpers.Nodes.NewsList();
+               if (newsProducts != null && newsProducts.Any())
+               {
+                  foreach (var newsProduct in newsProducts)
+                  {
+                     string newsProductTitle = newsProduct.GetValue<string>("newsProduct");
 
-                    newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsTopicsList") != null).ToList();
+                     NewsProductItem newsProductItem = new NewsProductItem();
 
-                    string newsTopicGuid = newsTopicFound.Value.ToLower();
+                     newsProductItem.Value = newsProduct.Id.ToString();
+                     newsProductItem.Text = newsProductTitle;
 
-                    //newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsTopicsList").ToLower().Contains(newsTopicGuid)).ToList();
+                     productList.Add(newsProductItem);
+                  }
 
-                    if (dateStart != null && dateEnd != null)
-                    {
-                        newsItems = newsItems.Where(p => p.GetPropertyValue<string>("newsTopicList") != null &&
-                            p.GetPropertyValue<string>("newsTopicsList").ToLower().Contains(newsTopicFound.Value.ToLower()) &&
-                            p.GetPropertyValue<DateTime>("articleDate") >= dateStart && p.GetPropertyValue<DateTime>("articleDate") <= dateEnd).ToList();
-                    }
-
-                    newsItems = newsItems.OrderByDescending(p => p.GetPropertyValue<DateTime>("articleDate")).ToList();
-
-                    newsItems = newsItems.Take(newsCount).ToList();
-                }
+               }
             }
+         }
 
-            return newsItems;
-        }
+         return productList;
+      }
 
 
-        public static List<NewsProductItem> GetNewsProductList()
-        {
-            List<NewsProductItem> productList = new List<NewsProductItem>();
-            IPublishedContent node = Helpers.Nodes.SiteSettings();
+      public static List<NewsTopicItem> GetNewsTopicList()
+      {
+         List<NewsTopicItem> topicList = new List<NewsTopicItem>();
+         IPublishedContent node = Helpers.Nodes.SiteSettings();
 
-            if (node != null)
+         if (node != null)
+         {
+            if (node.HasValue("newsTopics"))
             {
-                if (node.HasValue("newsProducts"))
-                {
-                    ArchetypeModel newsProducts = node.GetPropertyValue<ArchetypeModel>("newsProducts");
+               ArchetypeModel newsTopics = node.GetPropertyValue<ArchetypeModel>("newsTopics");
 
-                    if (newsProducts != null && newsProducts.Any())
-                    {
-                        foreach (var newsProduct in newsProducts)
-                        {
-                            string newsProductTitle = newsProduct.GetValue<string>("newsProduct");
+               if (newsTopics != null && newsTopics.Any())
+               {
+                  foreach (var newsTopic in newsTopics)
+                  {
+                     string newsTopicTitle = newsTopic.GetValue<string>("newsTopic");
 
-                            NewsProductItem newsProductItem = new NewsProductItem();
+                     NewsTopicItem newsTopicItem = new NewsTopicItem();
 
-                            newsProductItem.Value = newsProduct.Id.ToString();
-                            newsProductItem.Text = newsProductTitle;
+                     newsTopicItem.Value = newsTopic.Id.ToString();
+                     newsTopicItem.Text = newsTopicTitle;
 
-                            productList.Add(newsProductItem);
-                        }
+                     topicList.Add(newsTopicItem);
+                  }
 
-                    }
-                }
+               }
             }
+         }
 
-            return productList;
-        }
+         return topicList;
+      }
 
-
-        public static List<NewsTopicItem> GetNewsTopicList()
-        {
-            List<NewsTopicItem> topicList = new List<NewsTopicItem>();
-            IPublishedContent node = Helpers.Nodes.SiteSettings();
-
-            if (node != null)
-            {
-                if (node.HasValue("newsTopics"))
-                {
-                    ArchetypeModel newsTopics = node.GetPropertyValue<ArchetypeModel>("newsTopics");
-
-                    if (newsTopics != null && newsTopics.Any())
-                    {
-                        foreach (var newsTopic in newsTopics)
-                        {
-                            string newsTopicTitle = newsTopic.GetValue<string>("newsTopic");
-
-                            NewsTopicItem newsTopicItem = new NewsTopicItem();
-
-                            newsTopicItem.Value = newsTopic.Id.ToString();
-                            newsTopicItem.Text = newsTopicTitle;
-
-                            topicList.Add(newsTopicItem);
-                        }
-
-                    }
-                }
-            }
-
-            return topicList;
-        }
-
-    }
+   }
 }
