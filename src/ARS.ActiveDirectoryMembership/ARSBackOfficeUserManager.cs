8b4eeb3a18dec17bdd4fa252e5bcf8b5fbbeb1a3
John Skufca <john.skufca@imulus.com>
2016-08-29 14:27:23 -0600

    Issue #187: Add Support for multiple LDAP Connections

--- src/ARS.ActiveDirectoryMembership/ARSBackOfficeUserManager.cs
+++ src/ARS.ActiveDirectoryMembership/ARSBackOfficeUserManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.DirectoryServices;
+using System.Linq;
 using System.Threading.Tasks;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Models.Identity;
@@ -43,37 +45,52 @@ namespace ARS.ActiveDirectoryMembership
          bool resp = false;
          try
          {
-            string ldapRoot = ConfigurationManager.ConnectionStrings["ADConnectionString"].ConnectionString;
+            List<string> ldapArray = new List<string>();
 
-            LogHelper.Info(typeof(ARSBackOfficeUserManager), "Using LDAP: " + ldapRoot);
-
-            var entry = new DirectoryEntry(ldapRoot, username, password);
-            try
+            if (ConfigurationManager.ConnectionStrings["ADConnectionString"] != null &&
+                  false == string.IsNullOrWhiteSpace(ConfigurationManager.ConnectionStrings["ADConnectionString"].ConnectionString))
             {
-               LogHelper.Info(typeof(ARSBackOfficeUserManager), "Searching username: " + username);
+               ldapArray = ConfigurationManager.ConnectionStrings["ADConnectionString"].ConnectionString.Split(';').ToList();
 
-               var search = new DirectorySearcher(entry) { Filter = "(SAMAccountName=" + username + ")" };
-               search.PropertiesToLoad.Add("cn");
-               SearchResult result = search.FindOne();
-               if (result != null)
+               if (ldapArray != null && ldapArray.Any())
                {
-                  LogHelper.Info(typeof(ARSBackOfficeUserManager), "Username authenticated: " + username);
-                  // Login was successful
-                  resp = true;
+                  foreach (string ldapConnection in ldapArray)
+                  {
+                     if (resp == false)
+                     {
+                        LogHelper.Info(typeof(ARSBackOfficeUserManager), "Using LDAP: " + ldapConnection);
+
+                        var entry = new DirectoryEntry(ldapConnection, username, password);
+                        try
+                        {
+                           LogHelper.Info(typeof(ARSBackOfficeUserManager), "Searching username: " + username);
+
+                           var search = new DirectorySearcher(entry) { Filter = "(SAMAccountName=" + username + ")" };
+                           search.PropertiesToLoad.Add("cn");
+                           SearchResult result = search.FindOne();
+                           if (result != null)
+                           {
+                              LogHelper.Info(typeof(ARSBackOfficeUserManager), "Username authenticated: " + username);
+                              // Login was successful
+                              resp = true;
+                           }
+                           else
+                           {
+                              LogHelper.Warn(typeof(ARSBackOfficeUserManager), "Username NOT authenticated: " + username);
+                           }
+                        }
+                        catch (Exception ex)
+                        {
+                           LogHelper.Warn(typeof(ARSBackOfficeUserManager), "Problem with login: " + username);
+                        }
+                     }
+                  }
                }
-               else
-               {
-                  LogHelper.Warn(typeof(ARSBackOfficeUserManager), "Username NOT authenticated: " + username);
-               }
-            }
-            catch (Exception ex)
-            {
-               LogHelper.Warn(typeof(ARSBackOfficeUserManager), "Problem with login: " + username);
             }
          }
          catch (Exception ex)
          {
-            LogHelper.Warn(typeof(ARSBackOfficeUserManager), "Problem with login: " + username);
+            LogHelper.Warn(typeof(ARSBackOfficeUserManager), "Problem with ldap. Username: " + username);
          }
 
          return resp;
