dbc1860c7e9e364fab7a898f20ecd6fd727adcf4
John Skufca <john.skufca@imulus.com>
2016-09-02 06:54:53 -0600

    Update Templates to Handle Invalid Inputs From QueryString

---
+++ src/USDA-ARS.Umbraco.Extensions/Helpers/Logs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Logging;
+
+namespace USDA_ARS.Umbraco.Extensions.Helpers
+{
+   public class Logs
+   {
+      public static void AddErrorUrlToLog(Uri url)
+      {
+         if (url != null && url.AbsoluteUri != null)
+         {
+            LogHelper.Error(typeof(Logs), "ERROR URL: " + url.AbsoluteUri, null);
+         }
+      }
+   }
+}
